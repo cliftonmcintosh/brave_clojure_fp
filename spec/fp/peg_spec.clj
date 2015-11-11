c6b553b0b78b1c53fc6686d1bb0ceffd6024d4f1
Clifton McIntosh <cliff.mcintosh@gmail.com>
2015-11-11 08:43:05 -0600

    implementation and speclj specs for functions up to page 118 in Brave Clojure.

--- spec/fp/peg_spec.clj
+++ spec/fp/peg_spec.clj
@@ -25,4 +25,73 @@
               (should= { 1 {:connections {4 2}} 4 {:connections {1 2}}}
                        (connect {} 15 1 2 4))))
 
+(describe "connect-down-left"
+          (it "should make the right connections"
+              (should= {1 {:connections {4 2}}
+                        4 {:connections {1 2}}}
+                       (connect-down-left {} 15 1))))
+
+(describe "connect-down-right"
+          (it "should make the right connections"
+              (should= {3 {:connections {10 6}}
+                        10 {:connections {3 6}}}
+                       (connect-down-right {} 15 3))))
+
+(def simple-board
+  {1 {:connections {6 3, 4 2}, :pegged true}
+   4 {:connections {1 2}}
+   6 {:connections {1 3}}})
+
+(describe "add-pos"
+          (it "should identify the location of the peg"
+              (should= simple-board
+                       (add-pos {} 15 1))))
+
+(describe "pegged?"
+          (it "should return true when the position is pegged"
+              (should (pegged? simple-board 1)))
+          (it "should return false when the position is not pegged"
+              (should-not (pegged? simple-board 4))))
+
+(describe "remove-peg"
+          (it "should remove the peg from the specified position"
+              (should-not (get-in (remove-peg simple-board 1) [1 :pegged]))))
+
+(describe "place-peg"
+          (it "should put a peg in the specified position"
+              (should (get-in (place-peg (remove-peg simple-board 1) 4) [4 :pegged]))))
+
+(describe "move-peg"
+          (before-all
+           (def old-position 1)
+           (def new-position 4)
+           (def updated-board
+             (move-peg simple-board old-position new-position)))
+          (it "should remove the peg from the old position"
+              (should-not (get-in updated-board [old-position :pegged])))
+          (it "should put the peg in the new position"
+              (should (get-in updated-board [new-position :pegged]))))
+
+(describe "valid-moves"
+          (before-all
+           (def board (assoc-in (new-board 5) [4 :pegged] false)))
+          (it "should identify a valid move of {4 2} for position one"
+              (should= {4 2} (valid-moves board 1)))
+          (it "should identify a valid move of {4 5} for position six"
+              (should= {4 5} (valid-moves board 6)))
+          (it "should identify a valid move of {4 7} for position eleven"
+              (should= {4 7} (valid-moves board 11)))
+          (it "should identify no valid moves for position five"
+              (should= {} (valid-moves board 5)))
+          (it "should identify no valid moves for position eight"
+              (should= {} (valid-moves board 8))))
+
+(describe "valid-move?"
+          (before-all
+           (def board (assoc-in (new-board 5) [4 :pegged] false)))
+          (it "should return nil if a move is not valid"
+              (should-be-nil (valid-move? board 8 4)))
+          (it "should return the jumped position when the move is valid"
+              (should= 2 (valid-move? board 1 4))))
+
 (run-specs)
--- src/fp/peg.clj
+++ src/fp/peg.clj
@@ -36,4 +36,76 @@
             [[pos destination] [destination pos]])
     board))
 
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbor (inc pos)
+        destination (inc neighbor)]
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination)
+      board)))
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ row pos)
+        destination (+ 1 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ 1 row pos)
+        destination (+ 2 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn add-pos
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board connection-creation-fn]
+              (connection-creation-fn new-board max-pos pos))
+            pegged-board
+            [connect-right connect-down-left connect-down-right])))
+
+(defn new-board
+  "Creates a new board with the given number of rows"
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos] (add-pos board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
+
+(defn pegged?
+  [board pos]
+  (get-in board [pos :pegged]))
+
+(defn remove-peg
+  "Take the peg at given position out of the board"
+  [board pos]
+  (assoc-in board [pos :pegged] false))
+
+(defn place-peg
+  "Put a peg in the board at the given position"
+  [board pos]
+  (assoc-in board [pos :pegged] true))
+
+(defn move-peg
+  "Take peg out of one position and move it to another"
+  [board old-pos new-pos]
+  (place-peg (remove-peg board old-pos) new-pos))
+
+(defn valid-moves
+  "Returns a map of all valid moves for a position, where the key is the destination and the value is the jumped positions"
+  [board pos]
+  (into {}
+        (filter (fn [[destination jumped]]
+                  (and (not (pegged? board destination))
+                       (pegged? board jumped)))
+                (get-in board [pos :connections]))))
+
+(defn valid-move?
+  "Returns a jumped position if a move is valid, otherwise returns nil"
+  [board old-pos new-pos]
+  (get (valid-moves board old-pos) new-pos))
+
 (declare successful-move prompt-move game-over query-rows)
